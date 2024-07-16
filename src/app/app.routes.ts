932e302d48ca3e0fa5f9f5cb0a07f13c84bf8692
walter <nitlawalter69@gmail.com>
2024-07-15 21:54:01 -0300

    ajustes nas paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { ProcessosRegisterComponent } from './pages/processos-register/processos-register.component';
 import { Routes } from '@angular/router';
+import { ProcessosListComponent } from './pages/processos-list/processos-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'processos-list', pathMatch: 'full'},
+  {path: 'processos-list', component: ProcessosListComponent},
+  {path: 'processos-register', component: ProcessosRegisterComponent},
+];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  constructor(private router: Router ) { }
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
 
 }
